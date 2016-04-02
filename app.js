a40f6b41ac8de39d6cfcc04fd97b80dd1553d830
lucyyu24 <lucyyu1996@hotmail.com>
2016-04-02 11:23:20 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+app.get('/android-simulator', function(req, res){
+  res.sendfile('android-simulator.html');
+});
+
+var dashboard = io
+	.of('/dashboard')
+	.on('connection', function(socket){
+		console.log('a user connected');
+
+	});
+
+var android = io
+	.of('/android')
+	.on('connection', function(socket){
+		console.log('an android device has connected');
+		socket.on('alert', function(position){
+			console.log('an android device has submitted an alert');
+			dashboard.emit('map-update', position);
+		});
+	});
+
+// {lng:-74.6526860,lat:40.3503270}
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
