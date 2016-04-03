144152dbb553251e3768175c712ac3bab933b1cc
Matthew Tse <mtse9779@gmail.com>
2016-04-03 03:12:21 -0400

    works with 2 phones rather than 3

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ android = io
       noise.lng /= count;
       console.log(noise);
 
-      if (count > 2) {
+      if (count > 1) {
         console.log('3 ip in the buffer');
         dashboard.emit('noise-update', noise);
         for (var key in bufferObj) {
