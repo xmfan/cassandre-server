ed5babe0962889e73b220f152b9a14baa9b396c4
Simon Fan <Simon Fan>
2016-04-02 22:29:58 -0400

    updaing events

--- app.js
+++ app.js
@@ -21,11 +21,16 @@ var android = io
 	.on('connection', function(socket){
     var ip;
 		console.log('an android device has connected');
-		socket.on('alert', function(position){
-      ip = position.id;
+		socket.on('alert-location', function(ip, lat, lng){//alert-noise and alert-location
 			console.log('an android device has submitted an alert');
-			dashboard.emit('map-update', position);
+			console.log(ip+': '+lng+', ' + lat);
+			dashboard.emit('map-update', {id: ip, lng: lng, lat: lat}); 
 		});
+    socket.on('alert-noise', function(ip, dB){
+      console.log(ip + ' ' + dB);
+      //dashboard.emit('map-update', data.position);
+      dashboard.emit('noise-update', {ip: ip, volume: dB});
+    });
     socket.on('disconnect', function(message){
       console.log(ip);
 			console.log('android disconnected');
