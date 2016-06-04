202c61cc200ce9bef7dcd20dc292bfb2e18c36cd
Simon Fan <xmfan@uwaterloo.ca>
2016-06-04 01:54:06 -0400

    added comments and TODO tags

--- app.js
+++ app.js
@@ -2,34 +2,29 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-app.get('/android-simulator', function(req, res){
-  res.sendFile(__dirname + '/public/android-simulator.html');
-});
-
 var dashboard = io
 	.of('/dashboard')
 	.on('connection', function(socket){
-		console.log('a user connected');
+		console.log('Dashboard: Connection Established');
 	});
 
 var bufferObj = {};
 var android;
 android = io
 	.of('/android')
-	.on('connection', function(socket){
+	.on('connection', function(socket) {
     var ip;
-		console.log('an android device has connected');
-		socket.on('alert-location', function(ip, lat, lng){//alert-noise and alert-location
-			//console.log('an android device has submitted an alert');
-			console.log(ip+': '+lat+', ' + lng);
+		console.log('Android: Connection Established');
+
+    // Periodically update connected devices' location on the Dashboard
+		socket.on('alert-location', function(ip, lat, lng) {
+			console.log(ip + ': ' + lat + ', ' + lng);
 			dashboard.emit('map-update', {id: ip, lat: lat, lng: lng});
 		});
 
-    socket.on('alert-noise', function(ip, lat, lng, dB){
+    // TODO: Rewrite Algorithm
+    // Suspicious noise is detected and the server is alerted
+    socket.on('alert-noise', function(ip, lat, lng, dB) {
       bufferObj[ip] = {
         lat: lat,
         lng: lng,
@@ -40,6 +35,7 @@ android = io
         lat: 0,
         lng: 0
       };
+
       var count = 0;
       for (var ip in bufferObj) {
         noise.lat += bufferObj[ip].lat;
@@ -53,25 +49,36 @@ android = io
       if (count > 2) {
         console.log('3 ip in the buffer');
         dashboard.emit('noise-update', noise);
-        //emit coordinates to android
+        // emit coordinates to android
         android.emit('alert-map', noise);
         for (var key in bufferObj) {
           delete(bufferObj[key]);
         }
       }
     });
-    socket.on('disconnect', function(message){
+
+    socket.on('disconnect', function(message) {
       console.log(ip);
-			console.log('android disconnected');
+			console.log('Android: Connection Closed');
 			dashboard.emit('remove-marker', ip);
 		});
 	});
 
+// TODO: Put in Seperate files
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/android-simulator', function(req, res) {
+  res.sendFile(__dirname + '/public/android-simulator.html');
+});
+
+// Send evacuation instructions to all connected devices
 app.get('/evacuate', function(req, res) {
   android.emit('alert-evacuate');
   res.sendStatus(200);
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
