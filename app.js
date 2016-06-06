e2036642b36c524b9a20821bb404e95fc33ac7cb
Simon Fan <xmfan@uwaterloo.ca>
2016-06-05 21:55:00 -0400

    untabified

--- app.js
+++ app.js
@@ -6,28 +6,28 @@ const io = require('socket.io')(http);
 
 // TODO: move classes to other files
 const Coordinate = function(lat, lng) {
-	this.lat = lat;
-	this.lng = lng;
+  this.lat = lat;
+  this.lng = lng;
 };
 
 // Stored in alertMap, Alert destroys itself after >=3 seconds
 const Alert = function(lat, lng, dB, key, map) {
-	this.coord = new Coordinate(lat, lng);
-	this.dB = dB;
-	this.timestamp = key; // denormalized for faster shallow copy of map entries
-	this.timerID = (function() {
-		return setTimeout(selfDestruct, 3000);
+  this.coord = new Coordinate(lat, lng);
+  this.dB = dB;
+  this.timestamp = key; // denormalized for faster shallow copy of map entries
+  this.timerID = (function() {
+    return setTimeout(selfDestruct, 3000);
 
-		function selfDestruct() {
-			map.delete(key);
-		}
-	})();
+    function selfDestruct() {
+      map.delete(key);
+    }
+  })();
 };
 
 // Sound
 const SoundSource = function(lat, lng, W) {
-	this.coord = new Coordinate(lat, lng);
-	this.power = W;
+  this.coord = new Coordinate(lat, lng);
+  this.power = W;
 }
 
 // TODO: cluster sound events
@@ -35,42 +35,42 @@ const SoundSource = function(lat, lng, W) {
 const alertMap = new Map();
 
 const dashboard = io
-	.of('/dashboard');
+  .of('/dashboard');
 
 const android = io
-	.of('/android')
-	.on('connection', function(socket) {
+  .of('/android')
+  .on('connection', function(socket) {
 
-		// Periodically update connected devices' location on the Dashboard
-		socket.on('alert-location', function(ip, lat, lng) {
-			dashboard.emit('map-update', {id: ip, lat: lat, lng: lng});
-		});
+    // Periodically update connected devices' location on the Dashboard
+    socket.on('alert-location', function(ip, lat, lng) {
+      dashboard.emit('map-update', {id: ip, lat: lat, lng: lng});
+    });
 
-		// Alert the server when suspicious sound level is detected
+    // Alert the server when suspicious sound level is detected
     socket.on('alert-noise', function(ip, lat, lng, dB) {
-			socket.emit('alert-location', ip, lat, lng);
+      socket.emit('alert-location', ip, lat, lng);
 
-			const key = getCurrentTimestamp();
-			alertMap.set(key, new Alert(lat, lng, dB, key, alertMap));
-			// TODO: push alert to database
+      const key = getCurrentTimestamp();
+      alertMap.set(key, new Alert(lat, lng, dB, key, alertMap));
+      // TODO: push alert to database
 
-			// TODO: prevent redundant analysis
-			if (alertMap.size > 1) {
-				var source = traceSource(alertMap);
-				// TODO: log source to database
-				dashboard.emit('noise-update', source);
+      // TODO: prevent redundant analysis
+      if (alertMap.size > 1) {
+        var source = traceSource(alertMap);
+        // TODO: log source to database
+        dashboard.emit('noise-update', source);
         // emit coordinates of suspicious sound to android
         android.emit('alert-map', source);
-			}
+      }
     });
 
 
 
-		// Removes the disconnected devices' location marker
+    // Removes the disconnected devices' location marker
     socket.on('disconnect', function(ip, message) {
-			dashboard.emit('remove-marker', ip);
-		});
-	});
+      dashboard.emit('remove-marker', ip);
+    });
+  });
 
 // TODO: Put in Seperate files
 app.get('/', function(req, res) {
@@ -96,13 +96,13 @@ http.listen(3000, function() {
 ** Helper functions
 */
 function getCurrentTimestamp() {
-	return new Date().getTime();
+  return new Date().getTime();
 }
 
 function ascendingTimestamp(a, b) {
-	if (a.timestamp < b.timestamp) return -1;
-	if (a.timestamp > b.timestamp) return 1;
-	return 0;
+  if (a.timestamp < b.timestamp) return -1;
+  if (a.timestamp > b.timestamp) return 1;
+  return 0;
 }
 
 // returns distance in meters between 2 Coordinate objects
@@ -111,8 +111,8 @@ function distance(a, b){
   const dLat = (b.lat - a.lat) * Math.PI / 180;
   const dLon = (b.lng - a.lng) * Math.PI / 180;
   const c = Math.sin(dLat/2) * Math.sin(dLat/2) +
-  					Math.cos(a.lat * Math.PI / 180) * Math.cos(b.lat * Math.PI / 180) *
-  					Math.sin(dLon/2) * Math.sin(dLon/2);
+            Math.cos(a.lat * Math.PI / 180) * Math.cos(b.lat * Math.PI / 180) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
   const d = 2 * Math.atan2(Math.sqrt(c), Math.sqrt(1-c));
   const e = EARTH_RADIUS * d;
   return e * 1000;
@@ -120,57 +120,57 @@ function distance(a, b){
 
 // returns suspicious sound source's coordinates
 function traceSource(map) {
-	const SOUND_SPEED = 336; // speed of sound in NTP conditions, in meters per second
+  const SOUND_SPEED = 336; // speed of sound in NTP conditions, in meters per second
 
-	// shallow copy of self-destructing alerts in map of alerts
-	// TODO: replace this with fail-safe query by timestamp key
-	var alerts = new Array();
-	for (var alert of map.entries()) {
-		alerts.push(alert);
-	}
+  // shallow copy of self-destructing alerts in map of alerts
+  // TODO: replace this with fail-safe query by timestamp key
+  var alerts = new Array();
+  for (var alert of map.entries()) {
+    alerts.push(alert);
+  }
 
-	// sorting coordinates by timestamp of arrival
-	alerts.sort(ascendingTimestamp);
+  // sorting coordinates by timestamp of arrival
+  alerts.sort(ascendingTimestamp);
 
-	// Estimation Algorithm revolves on Interaural Time Difference and Interaural Level Difference theories
-	// Assumptions:
-	//   1) all alerts in alertsMap at a certain time are originating from the same sound source
-	//   2) the lower the timestamp, the closer the alert's coordinate is to the sound source (applying Interaural Time Difference)
-	//   3) near no reflections nor reverberation in open spaces (applying Inverse Square Law for Sound)
-	//   4) inteference of noise does not affect sound speed
-	//   5) the open space is at the sea level of a WGS84 spheroid-shaped Earth
-
-
-	// Step 1. Convert intensity from dB to W/m^2
-	// intensity(W/m^2) = threshold of hearing(W/m^2) * 10^(1/10 * intensity(dB))
-	//   where threshold of hearing(W/m^2) = 10^(-12)
-	for (var i=0; i<alerts.length; i++) {
-		alerts[i].intensity = 10^(-12) * 10^(alerts[i].dB/10);
-	}
-
-	// Array of all possible sound source Coordinate objects
-	var possibleSources = new Array();
-
-	// Step 2. Using speed of sound and Interaural Time Difference
-	for (var i=0; i<alerts.length; i++) {
-		const a = alerts[i];
-		for (var j=i+1; j<alerts.length; j++) {
-			const b = alerts[j];
-			const time = b.timestamp - a.timestamp;
-			const projdist = time * SOUND_SPEED; // distance between scalar projection of a on b and b, in meters
-			const dist = distance(a.coord, b.coord); // distance between a and b, in meters
-
-			// Step 3. Apply Inverse Square Law
-			// minimize dist by moving a's coordinates along the circumference of the circle centered at the sound source (SS)
-			// use the maximum of the derivative of the distance function,
-			// with respect to the angle between vectors from SS to a and from SS to b.
-			// let somelat and somelng represent the coordinates of SS
-			var somelat, somelng;
-			const source = new Coordinate(somelat, somelng);
-			possibleSources.push(source);
-		}
-	}
+  // Estimation Algorithm revolves on Interaural Time Difference and Interaural Level Difference theories
+  // Assumptions:
+  //   1) all alerts in alertsMap at a certain time are originating from the same sound source
+  //   2) the lower the timestamp, the closer the alert's coordinate is to the sound source (applying Interaural Time Difference)
+  //   3) near no reflections nor reverberation in open spaces (applying Inverse Square Law for Sound)
+  //   4) inteference of noise does not affect sound speed
+  //   5) the open space is at the sea level of a WGS84 spheroid-shaped Earth
 
 
-	return possibleSources;
+  // Step 1. Convert intensity from dB to W/m^2
+  // intensity(W/m^2) = threshold of hearing(W/m^2) * 10^(1/10 * intensity(dB))
+  //   where threshold of hearing(W/m^2) = 10^(-12)
+  for (var i=0; i<alerts.length; i++) {
+    alerts[i].intensity = 10^(-12) * 10^(alerts[i].dB/10);
+  }
+
+  // Array of all possible sound source Coordinate objects
+  var possibleSources = new Array();
+
+  // Step 2. Using speed of sound and Interaural Time Difference
+  for (var i=0; i<alerts.length; i++) {
+    const a = alerts[i];
+    for (var j=i+1; j<alerts.length; j++) {
+      const b = alerts[j];
+      const time = b.timestamp - a.timestamp;
+      const projdist = time * SOUND_SPEED; // distance between scalar projection of a on b and b, in meters
+      const dist = distance(a.coord, b.coord); // distance between a and b, in meters
+
+      // Step 3. Apply Inverse Square Law
+      // minimize dist by moving a's coordinates along the circumference of the circle centered at the sound source (SS)
+      // use the maximum of the derivative of the distance function,
+      // with respect to the angle between vectors from SS to a and from SS to b.
+      // let somelat and somelng represent the coordinates of SS
+      var somelat, somelng;
+      const source = new Coordinate(somelat, somelng);
+      possibleSources.push(source);
+    }
+  }
+
+
+  return possibleSources;
 }
